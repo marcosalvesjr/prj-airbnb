5dfb2e4734b63e5a68fa85140e7aee489ff2a489
Marcos Alves <mrcaurelio95@gmail.com>
2025-04-21 07:45:53 -0300

    Update: AccomodationsTestemunials

--- airbnb/src/widgets/AccomodationDetails/AccomodationDetails.tsx
+++ airbnb/src/widgets/AccomodationDetails/AccomodationDetails.tsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import {
+  IconPhoto,
+  IconToolsKitchen2,
+  IconDesk,
+  IconPool,
+  IconBrandCarbon,
+  IconSailboat,
+  IconWifi,
+  IconParking,
+  IconAlarmSmoke,
+  IconDog,
+} from "@/assets/icons";
 
 const AccomodationDetails = () => {
   return (
-    <div>AccomodationDetails</div>
-  )
-}
+    <div className="w-full py-4">
+      <h2 className="pt-4 text-xl font-semibold">
+        Espaço inteiro: casa de campo em Minas Gerais
+      </h2>
+      <ul className="flex gap-y-2 items-center">
+        <li>10 hospodes</li>
+        <li>&middot;</li>
+        <li>5 quartos</li>
+        <li>&middot;</li>
+        <li>5 camas</li>
+        <li>&middot;</li>
+        <li>6 banheiros</li>
+      </ul>
 
-export default AccomodationDetails
+      <h2 className="py-4 text-xl font-semibold">O que esse lugar oferece?</h2>
+      <ul className="py-4 grid grid-cols-2 gap-4 w-full">
+        <li className="flex gap-2">
+          <IconPhoto />
+          <span>Vista apra as montanhas</span>
+        </li>
+        <li className="flex gap-2">
+          <IconDesk />
+          <span>Espaço de trabalho exclusivo</span>
+        </li>
+        <li className="flex gap-2">
+          <IconBrandCarbon />
+          <span>Alarme de segurança</span>
+        </li>
+        <li className="flex gap-2">
+          <IconWifi />
+          <span>Wi-Fi rápido (83Mbps)</span>
+        </li>
+        <li className="flex gap-2">
+          <IconDog />
+          <span>Permitido animais</span>
+        </li>
+        <li className="flex gap-2">
+          <IconToolsKitchen2 />
+          <span>Cozinha</span>
+        </li>
+        <li className="flex gap-2">
+          <IconPool />
+          <span>Piscina privativa</span>
+        </li>
+        <li className="flex gap-2">
+          <IconSailboat />
+          <span>Vista para as águas</span>
+        </li>
+        <li className="flex gap-2">
+          <IconParking />
+          <span>Estacionamento incluído</span>
+        </li>
+        <li className="flex gap-2">
+          <IconAlarmSmoke />
+          <span>Detector de fumaça</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AccomodationDetails;
--- airbnb/src/widgets/AccomodationTestemunials/AccomodationTesteminials.tsx
+++ airbnb/src/widgets/AccomodationTestemunials/AccomodationTesteminials.tsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { IconStarFilled } from "@/assets/icons";
+import Image from "next/image";
 
 const AccomodationTestemunials = () => {
-  return <div>AccomodationTestemunials</div>;
+  const testemunials = [
+    {
+      id: "560b7417-4280-4afc-ae1d-9159934fde2a",
+      name: "Eufrásio",
+      image:
+        "https://web.codans.com.br/airbnb/img/feedback/560b7417-4280-4afc-ae1d-9159934fde2a.jpg",
+      comment: "Bacon ipsum dolor amet tenderloin swine t-bone flank.",
+      rating: 5,
+      customerTime: 5,
+      createdAt: 7,
+      stayedAt: 2,
+    },
+    {
+      id: "63901507-1bf2-4e49-8798-eeaf2f4fc0a1",
+      name: "Raquélen",
+      image:
+        "https://web.codans.com.br/airbnb/img/feedback/63901507-1bf2-4e49-8798-eeaf2f4fc0a1.jpg",
+      comment:
+        "Bacon ipsum dolor amet tenderloin pastrami brisket bresaola, ham burgdoggen chicken shankle swine strip steak venison frankfurter.",
+      rating: 5,
+      customerTime: 2,
+      createdAt: 7,
+      stayedAt: 8,
+    },
+    {
+      id: "df3745f0-175e-4831-9ac7-b94ae7e76c1f",
+      name: "Cristina",
+      image:
+        "https://web.codans.com.br/airbnb/img/feedback/df3745f0-175e-4831-9ac7-b94ae7e76c1f.jpg",
+      comment:
+        "Bacon ipsum dolor amet strip steak pork belly spare ribs, bacon tenderloin sausage cow.",
+      rating: 3.5,
+      customerTime: 4,
+      createdAt: 5,
+      stayedAt: 2,
+    },
+  ];
+
+  return (
+    <div className="w-full py-4">
+      <h2 className="text-xl font-semibold">Depoimentos</h2>
+      <div className="flex gap-1.5 items-center">
+        <IconStarFilled className="size-4" />
+        <span>4.9 (+400 avaliações)</span>
+      </div>
+      {testemunials.map((testemunial, index) => (
+        <div key={index} className="flex gap-4 items-start py-4">
+          <Image
+            className="aspect-square object-cover rounded-full"
+            src={testemunial.image}
+            alt={testemunial.name}
+            width={48}
+            height={48}
+          />
+          <div>
+            <h4 className="font-semibold">{testemunial.name}</h4>
+            <span>{testemunial.comment}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default AccomodationTestemunials;
