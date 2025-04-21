379c655276a3be4820589ae6931b1f4af8cea121
Marcos Alves <mrcaurelio95@gmail.com>
2025-04-21 08:10:29 -0300

    Create: AirBnbDates

---
+++ airbnb/src/types/AirBnbDates.ts
@@ -0,0 +1,43 @@
+export type AirBnbApi = {
+  accommodation: Accommodation[];
+  icons: Photo[];
+};
+
+export type Accommodation = {
+  id: string;
+  tittle: string;
+  date: string;
+  testimonials: Testimonials[];
+  hasBadge: boolean;
+  host: string;
+  slug: string;
+  location: Location;
+  price: number;
+  rating: number;
+  photos: Photo[];
+};
+
+export type Location = {
+  description: string;
+  city: string;
+  state: string;
+  country: string;
+};
+
+export type Testimonials = {
+  id: string;
+  name: string;
+  image: string;
+  comment: string;
+  rating: number;
+  customerTime: number;
+  createAt: number;
+  stayedAt: number;
+};
+
+export type Photo = {
+  id: string;
+  source: string;
+  description: string;
+  url: string;
+};
